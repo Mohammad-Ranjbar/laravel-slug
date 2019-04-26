2597af19564fc1ac3a803e9e2cce6877c2ddde45
MohammadJavad <mj.ranjbar.94@gmail.com>
2019-04-26 14:14:34 -0400

    Refactoring

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -22,46 +22,7 @@ class PostsController extends Controller
     {
         $post = new Post;
 
-        $post->title = $request->title;
-        $post->body = $request->body;
-
-        $post->slug = str_slug($request->title);
-        /* slug with regular expression
-
-//        $index = 1;//N+1 problem
-//        while(Post::whereSlug($post->slug)->exists()){
-//
-//            $post->slug = str_slug($request->title).'-'.$index++;
-//        }
-
-        $count = Post::whereRaw("slug RLIKE '^{$slug}(-[0-9]*)?$'")->count();
-
-
-//        if ($count){
-//
-//            $post->slug = "{$slug}-{$count}";
-//
-//        }
-//
-//        $post->slug = $slug;
-
-//        $post->slug = $count ? "{$slug}-{$count}" : $slug;
-*/
-       $latestSlug = Post::whereRaw("slug RLIKE '^{$post->slug}(-[0-9]*)?$'")
-
-                    ->orderBy('id')
-                    ->pluck('slug');
-
-        if ($latestSlug){
-
-            $pieces = explode('-',$latestSlug);
-            $number = intval(end($pieces));
-            $post->slug .='-'.($number+1);
-
-        }
-
-        $post->save();
-//$post->makeSlug($request);
+        $post->makeSlug($request);
         return $post;
     }
 
--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,48 @@ use Illuminate\Http\Request;
 
 class Post extends Model
 {
-    //
+
+
+    public function makeSlug( $request)
+    {
+        $this->title = $request->title;
+        $this->body = $request->body;
+        $this->slug = str_slug($request->title);
+        /* slug with regular expression
+
+//        $index = 1;//N+1 problem
+//        while(Post::whereSlug($post->slug)->exists()){
+//
+//            $post->slug = str_slug($request->title).'-'.$index++;
+//        }
+
+        $count = Post::whereRaw("slug RLIKE '^{$slug}(-[0-9]*)?$'")->count();
+
+
+//        if ($count){
+//
+//            $post->slug = "{$slug}-{$count}";
+//
+//        }
+//
+//        $post->slug = $slug;
+
+//        $post->slug = $count ? "{$slug}-{$count}" : $slug;
+*/
+        $latestSlug = Post::whereRaw("slug RLIKE '^{$this->slug}(-[0-9]*)?$'")
+
+            ->orderBy('id')
+            ->pluck('slug');
+
+        if ($latestSlug){
+
+            $pieces = explode('-',$latestSlug);
+            $number = intval(end($pieces));
+            $this->slug .='-'.($number+1);
+
+        }
+
+        $this->save();
+    }
+
 }
